974b1e1e0d4fd1d149819cf067c07f09b22223f3
geekboyisonnow <kennethfcannon@gmail.com>
2019-03-02 23:58:25 -0500

    Admin Database Complete

--- db/migrate/20190302165812_create_goals.rb
+++ db/migrate/20190302165812_create_goals.rb
@@ -2,7 +2,7 @@ class CreateGoals < ActiveRecord::Migration[5.2]
   def change
     create_table :goals do |t|
       t.string :goal_name
-      t.decimal :goal_amount
+      add_monetize :goal_amount
       t.references :customer, foreign_key: true
 
       t.timestamps
--- db/migrate/20190302165834_create_deposits.rb
+++ db/migrate/20190302165834_create_deposits.rb
@@ -1,7 +1,7 @@
 class CreateDeposits < ActiveRecord::Migration[5.2]
   def change
     create_table :deposits do |t|
-      t.decimal :deposit_amount
+      add_monetize :deposit_amount
       t.references :goal, foreign_key: true
 
       t.timestamps
